30ad98ccb83311fb46e2a254a453532b1cf2d57d
RainBell98 <ttiger45@naver.com>
2023-08-17 16:23:01 +0900

    aws

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const db = require('./models/index')
 
-const express = require("express")
-const app = express();
-const PORT = 8000;
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set('view engine','ejs')
+app.set('views','./views')
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 //router
-import userRouter from './routes/user.js'
-app.use('/user', userRouter);
+// const indexRouter = require('./routes')//"./routes <-라고 써도 됨"
+// app.use('/',indexRouter)
+//ex)
+//const userRouter = require('./rotes/user)
+//app.use('/user',userRouter)
+const visitorRouter = require('./routes/visitor')
 
-//404
-app.use('*', (req, res) => {
-    res.render('404');
-});
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+//localhost:8000/visitor
+app.use('/visitor',visitorRouter)
 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-});
+
+//마지막 선언
+app.use('*',(req,res)=>{
+    res.render('404')
+})
+
+
+db.sequelize.sync({force:false}).then(()=>{
+    app.listen(port,()=>{
+        console.log(`http://localhost:${port}`)
+    })
+})
